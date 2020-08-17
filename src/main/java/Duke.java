a3056d57c0e220af43a933875c2b85f3555783e8
Zhixiang <zhixiangteoh@hotmail.com>
2020-08-17 12:43:18 +0800

    add level 0 greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+    final static String border = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____            _\n"
+                    + "|  _ \\ _   _   _| | ___\n"
+                    + "| | | | | | |/ _  |/ _ \\\n"
+                    + "| |_| | |_| | |_| |  __/\n"
+                    + "|____/ \\__,_|\\____|\\___|";
         System.out.println("Hello from\n" + logo);
+        System.out.println(border);
+        greet();
+        farewell();
+    }
+
+    private static void greet() {
+        String greeting = "Hello! I'm Dude\n"
+                        + "What can I do for you?";
+        System.out.println(greeting);
+        System.out.println(border);
+    }
+
+    private static void farewell() {
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(farewell);
+        System.out.println(border);
     }
 }
